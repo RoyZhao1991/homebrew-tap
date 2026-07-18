015b3a4ff2f19565d4c5d28085077fe4aa349598
yang zhao <2537831952@qq.com>
2026-07-18 13:55:46 +0800

    chore: update LingShu cask to alpha.6

--- Casks/lingshu.rb
+++ Casks/lingshu.rb
@@ -1,8 +1,8 @@
 cask "lingshu" do
-  version "0.1.0-8"
-  sha256 "a8b99ea48b345f25af416616ea5d6dd37f4ff88114e9677b9cc6a27d36dd87e4"
+  version "0.1.0-9"
+  sha256 "1e3ad6b3311e69c6e83e2da69b3b049eeb400ace81e0d8b82dc99812fd2d7d81"
 
-  url "https://github.com/RoyZhao1991/LingShu/releases/download/v0.1.0-alpha.5/LingShu-#{version}-macOS-universal.dmg"
+  url "https://github.com/RoyZhao1991/LingShu/releases/download/v0.1.0-alpha.6/LingShu-#{version}-macOS-universal.dmg"
   name "LingShu"
   name "灵枢"
   desc "AI agent that turns goals into verified deliverables"
